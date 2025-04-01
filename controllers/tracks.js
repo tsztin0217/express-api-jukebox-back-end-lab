baad9464fa484d2dd816a0b724b6a1ada86aec4b
tsztin0217 <tsztin0217@gmail.com>
2025-04-01 03:31:11 -0400

    Add get and put route for indivdual tracks

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -22,5 +22,30 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const track = await Track.findById(req.params.id);
+        res.status(200).json(track);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const track = await Track.findById(req.params.id);
+
+        const updatedTrack = await Track.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true}
+        )
+        res.status(200).json(track);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message});
+    }
+});
 
 module.exports = router;
